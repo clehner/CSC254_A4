8ad5ce69265bb15ec04cdcc17b5d8fe3fbe520f8
Daniel Scarafoni <dscarafo@u.rochester.edu>
2013-11-09 16:00:31 -0500

    same page highlighting draft

--- static/highlight.js
+++ static/highlight.js
@@ -1,4 +1,7 @@
-window.onload = function(){
+window.onload = hash_change;
+window.onhashchange = hash_change;
+
+function hash_change(){
 	if(location.hash.substr(1) && document.getElementById(location.hash.substr(1)))
 		document.getElementById(location.hash.substr(1)).parentNode.className = 'highlight';
-};
+}
